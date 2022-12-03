232d552e9453f103fd36732f6c71c22c77dbfacc
RoshanKumar81 <roshankumarrtk81@gmail.com>
2022-12-03 12:35:10 +0530

    changed about

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -5,10 +5,11 @@ export const About = () =>{
         <div>
             This is an About Component.
             <p>
-                Lorem iqsum dolor sit amet consectetur,adipisicing elit. Cum corporis
-                odio,nesciunt sed eveniet,nemo amet mdignissimos magnnan itaque iqsam
-                possimus.Inventore vitae, perderendis harum queas cupidiatate adipisici
-                repellendus tempore asperiores at consequatur quos?
+            ToDo List App is a kind of app that generally used to maintain our day-to-day tasks 
+            or list everything that we have to do. 
+            When we have a better sense of the estimated time we’ll need to spend on our tasks, 
+            along with the validated time spent on the items for reference or personal/team reviews, 
+            we are able to manage our daily routines more efficiently.
             </p>
         </div> 
     )
